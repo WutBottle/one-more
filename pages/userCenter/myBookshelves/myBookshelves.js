40522f6560a528e884ea8e4b4c8e19bffc9b58e8
WYH-han <382839205@qq.com>
2018-09-27 13:37:03 +0800

    完成个人中心部分 子页面待开发

---
+++ pages/userCenter/myBookshelves/myBookshelves.js
@@ -0,0 +1,78 @@
+// pages/userCenter/myBookshelves/myBookshelves.js
+var app = getApp()
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    if (app.globalData.openId) {
+      wx.request({
+        url: app.globalData.requestRoot +'/onemore/bookShelf/findAll',
+        method:"POST",
+        data:{
+          uid:app.globalData.userInfo,
+        },
+        success:res =>{
+          console.log(res);
+        }
+      })
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
