a629475bd998beab231625bcb2b9d069d5bed6f0
WYH-han <382839205@qq.com>
2018-10-03 19:09:47 +0800

    书架页面接口连接完成 逻辑待修改

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ App({
               }
               //登录接口
               userLogin(param).then((data) => {
-                this.uid = data.uid;
+                this.globalData.uid = data.uid;
               })
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
               // 所以此处加入 callback 以防止这种情况
