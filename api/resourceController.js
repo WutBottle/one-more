1b3cb1f3c25d678defa0182533cad6b3d7846a93
WYH-han <382839205@qq.com>
2018-10-03 22:56:14 +0800

    完成编写剩余的接口文件&个人中心基本信息排版修改&我的书架逻辑修改（待测试）

--- api/resourceController.js
+++ api/resourceController.js
@@ -8,4 +8,36 @@ module.exports = {
   addAudio: (data) => {
     return resourceController('post', 'resource', 'addAudio', data);
   },
+  //2.添加一个图片
+  addAudio: (data) => {
+    return resourceController('post', 'resource', 'addImg', data);
+  },
+  //3.添加一条资源
+  addAudio: (data) => {
+    return resourceController('post', 'resource', 'addResource', data);
+  },
+  //4.查找更多资源（可分页）
+  addAudio: (data) => {
+    return resourceController('post', 'resource', 'findAllByTime', data);
+  },
+  //5.查找资源详情页面
+  addAudio: (data) => {
+    return resourceController('post', 'resource', 'findInfo', data);
+  },
+  //6.查找资源详情页面（最新的评论）
+  addAudio: (data) => {
+    return resourceController('post', 'resource', 'findInfoNew', data);
+  },
+  //7.查找资源详情页面（自己的评论）
+  addAudio: (data) => {
+    return resourceController('post', 'resource', 'findInfoSend', data);
+  },
+  //8.资源首页初始化
+  addAudio: (data) => {
+    return resourceController('post', 'resource', 'findResourceIndex', data);
+  },
+  //9.查找某人发表评论的页面（可分页）
+  addAudio: (data) => {
+    return resourceController('post', 'resource', 'selectByPublish', data);
+  },
 }
